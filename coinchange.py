6a0317950d5acf657a63da0598abf479bb5f9d7d
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-04-17 22:04:55 -0700

    added new code to file

--- coinchange.py
+++ coinchange.py
@@ -1,3 +1,4 @@
+#/usr/bin/python3
 price_amount = 1.30
 
 quarters = .50
@@ -5,17 +6,11 @@ nickle = .05
 penny = .01
 fifty_piece = .50
 
-thisdict = {'quarters': 10, 'nickle': 5, 'penny': 6, 'fifty_piece': 1}
-
-quarters_p = ["quarters"]
-
-num = []
+mylist = ["quarters", "nickle", "penny", "fifty_piece"]
 
 i = 1
-while i < 2:
-    for i in thisdict:
-       num = quarters + nickle + penny + fifty_piece # add up key int's
-       print(num)
-       if i == 2:
-           break
-       i += 2
+while True:
+    print(mylist)
+    break
+
+num = []
